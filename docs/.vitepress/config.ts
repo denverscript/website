ad4a894482d54d1bfa74159a136276a0ef7db3cf
Cassandra <cassandra.torske@gmail.com>
2024-07-23 15:59:19 -0600

    add july news (#7)
    * add july news

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -21,6 +21,9 @@ export default defineConfig({
       '/news/': [{
         text: 'JS News Archive',
         items: [{
+          text: "July 2024",
+          link: "/news/2024-07.md",
+        },{
           text: "June 2024",
           link: "/news/2024-06.md",
         },{
